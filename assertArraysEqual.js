3754cc011a128c4dea0cf4fcb1171fb3a4db0afa
Joseph McGregor <joseph00@my.yorku.ca>
2021-12-01 08:11:20 -0500

    assert arrays equal function

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+  
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅  Assertion passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]))
