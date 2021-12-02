0ce412b4cb3ceffac82877ecd5c2f251f8e3e72d
Joseph McGregor <joseph00@my.yorku.ca>
2021-12-02 15:39:30 -0500

    how many times a letter shows up

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (sentence) {
+  
+  let result = {};
+  sentence = sentence.replace(/\s/g, "")
+  for (letter of sentence) {
+    if (result[letter]) {
+      result[letter]++
+    } else {
+      result[letter] = 1
+    }
+  }
+
+
+  return result;
+}
+
+console.log(countLetters("string is string"))
