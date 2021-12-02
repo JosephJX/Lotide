8eba3c0d40f43177b55e3c8dbf15c6e3623f25f9
Joseph McGregor <joseph00@my.yorku.ca>
2021-12-02 09:35:33 -0500

    refactored to just the function

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,3 @@
-const assertEqual = require("./assertEqual")
-
 const eqArrays = (arr1, arr2) => {
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
@@ -15,10 +13,3 @@ const eqArrays = (arr1, arr2) => {
 };
 
 module.exports = eqArrays;
-
-// console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-// console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]));// => true
-
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]))// => 
