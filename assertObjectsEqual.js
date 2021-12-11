7ccbf38ebb78402ff6e654987bf3d25b91dfe9a3
Joseph McGregor <joseph00@my.yorku.ca>
2021-12-11 13:37:00 -0500

    save

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -33,8 +33,8 @@ const assertObjectEqual = function(object1, object2) {
   }
 };
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertObjectEqual(cd, dc);
-assertObjectEqual(cd, cd2); 
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertObjectEqual(cd, dc);
+// assertObjectEqual(cd, cd2); 
