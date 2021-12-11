5ea01aa47a55601906f64b64e5e052acd7a633ca
Joseph McGregor <joseph00@my.yorku.ca>
2021-12-11 18:49:35 -0500

    implemented findKey

---
+++ findKey.js
@@ -0,0 +1,24 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(key, callback) {
+  for (let obj in key) {
+    if (callback(key[obj])) {
+      return obj;
+    }
+  }
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3))// => "noma
