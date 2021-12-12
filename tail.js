76700c968f34cc37fb57ee28d87ca19cf7a5c00c
Joseph McGregor <joseph00@my.yorku.ca>
2021-12-12 12:31:10 -0500

    refactored with mocha and chai

--- tail.js
+++ tail.js
@@ -2,7 +2,7 @@ const assertEqual = require("./assertEqual")
 const eqArrays = require("./eqArrays")
 
 const tail = (arr) => {
-  return arr.slice(1);
+  return arr[arr.length - 1];
 };
 
 module.exports = tail;
