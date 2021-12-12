53892e1974be3cb797fa1dd7c453822e83c56338
Joseph McGregor <joseph00@my.yorku.ca>
2021-12-12 13:16:29 -0500

    incorporated mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,29 @@
-const assertEqual = require("../assertEqual")
+// const assertEqual = require("../assertEqual")
 
-const eqArrays = require("../eqArrays")
+// const eqArrays = require("../eqArrays")
 
-const tail = require("../tail")
+// const tail = require("../tail")
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
 
-console.log(result);
+// console.log(result);
+// const assertEqual = require("../assertEqual")
+const assert = require('chai').assert;
+const tail = require('../tail');
 
-assertEqual(eqArrays(result, ["Lighthouse", "Labs"]), true);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// console.log(tail(words));
+// assertEqual(words.length, 3);
+// assertEqual(tail(words).length, 2);
+// assertEqual(tail(words).length, 2)
+
+
+describe("#tail", () => {
+
+it("returns 3 for [1, 2, 3]", () => {
+ 
+  assert.strictEqual(tail([1, 2, 3]), 3);
+});
+
+});
+;
