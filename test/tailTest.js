3fc067983db650dcb3ea4a6ae7009723ed9bd06f
Joseph McGregor <joseph00@my.yorku.ca>
2021-12-12 13:17:17 -0500

    removed comments

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,22 +1,5 @@
-// const assertEqual = require("../assertEqual")
-
-// const eqArrays = require("../eqArrays")
-
-// const tail = require("../tail")
-
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-
-// console.log(result);
-// const assertEqual = require("../assertEqual")
 const assert = require('chai').assert;
 const tail = require('../tail');
-
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// console.log(tail(words));
-// assertEqual(words.length, 3);
-// assertEqual(tail(words).length, 2);
-// assertEqual(tail(words).length, 2)
-
 
 describe("#tail", () => {
 
