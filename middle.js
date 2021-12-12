11f1f0ed6fa0ae3d23c574483ab9135a938f9cbc
Joseph McGregor <joseph00@my.yorku.ca>
2021-12-12 13:34:27 -0500

    return an array

--- middle.js
+++ middle.js
@@ -3,24 +3,26 @@ const eqArrays = require("./eqArrays")
 const assertArraysEqual = require("./assertArraysEqual");
 
 const middle = (arr) => {
+  const newArray = [];
   if (arr.length <= 2) {
     return [];
   } else if (arr.length % 2 !== 0) {
     let formulaOdd = Math.floor(arr.length / 2);
-    console.log(arr[formulaOdd]);
-    return arr[formulaOdd];
+    newArray.push(arr[formulaOdd])
+    return newArray;
   } else {
     let formulaEven = (arr.length / 2);
-    console.log(arr[formulaEven - 1], arr[formulaEven]);
-    return arr[formulaEven];
+    newArray.push(arr[formulaEven - 1]) 
+    newArray.push(arr[formulaEven])
+    return newArray;
   }
 };
 
-module.export = middle;
+module.exports = middle;
 
 // Test Code
 // middle([1, 2, 3]) // => [2]
 // middle([1, 2, 3, 4, 5])
 
-// middle([1, 2, 3, 4]) // => [2, 3]
-// middle([1, 2, 3, 4, 5, 6])
+middle([1, 2, 3, 4]) // => [2, 3]
+middle([1, 2, 3, 4, 5, 6])
