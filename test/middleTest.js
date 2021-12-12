66db00bfbcaad80448e59e514253c92a42404654
Joseph McGregor <joseph00@my.yorku.ca>
2021-12-12 13:34:51 -0500

    incorporate mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,23 @@
-const assertArraysEqual = require("../assertArraysEqual")
+// const assertArraysEqual = require("../assertArraysEqual")
 
-const middle = require("../middle")
+// const middle = require("../middle")
+// const assertEqual = require("../assertEqual")
+const assert = require('chai').assert;
+const middle = require('../middle');
 
-middle([1, 2, 3]) 
-middle([1, 2, 3, 4, 5])
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]); 
+  });
+});
 
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6])
+
+// // middle([1, 2, 3]) 
+// // middle([1, 2, 3, 4, 5])
+
+// // middle([1, 2, 3, 4]) // => [2, 3]
+// // middle([1, 2, 3, 4, 5, 6])
+// 
