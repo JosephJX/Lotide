4b95e925d687887d85e18d4b558ac0f401061e85
Joseph McGregor <joseph00@my.yorku.ca>
2022-01-18 14:31:38 -0500

    refactored

--- without.js
+++ without.js
@@ -1,23 +1,6 @@
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅  Assertion passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${arr1} !== ${arr2}`);
-  }
-};
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
+const assertArraysEqual = require('./assertArraysEqual')
 
-};
+const eqArrays = require('./eqArrays')
 
 const without = (source, itemsToRemove) => {
   let newArray = [];
@@ -26,11 +9,15 @@ const without = (source, itemsToRemove) => {
       newArray.push(item);
     }
   }
- return newArray;
+  return newArray;
 
 };
-// !arr1.includes(item)
-assertArraysEqual(without([1, 2, 3], [2, 3]), [1]);
 
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+module.exports = without;
+
+
+// !arr1.includes(item)
+// assertArraysEqual(without([1, 2, 3], [2, 3]), [1]);
+
+// assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
 
